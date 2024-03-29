49f73d378b87ca377e0cc7fbdb67ad594cd32eaf
Faustino Alfredo <faustinoalfredo06@gmail.com>
2024-03-29 22:49:16 +0100

    Mise en place de Thymeleaf

---
+++ src/test/java/com/kubanza/thymeleaf/ThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubanza.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
